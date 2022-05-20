39804fd3c478562298335b6b3d535f5290911a63
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2022-05-20 21:13:29 +0500

    Settings page switch

--- src/js/components/settingsPageChange.js
+++ src/js/components/settingsPageChange.js
@@ -1,14 +1,26 @@
-$(function(){
-    const pages = $('.js-settings-pages')
+$(function () {
+  const buttons = Object.assign([], $('.js-settings-button'));
+  const pages = Object.assign([], $('.js-settings-page'));
 
-    let targetPage;
+  const changePage = (target) => {
+    let currentPage;
 
-    const changePage = (target) => {
+    pages.forEach((item, index) => {
+      item.classList.remove('active');
+      if (item.classList.contains(target)) {
+        currentPage = index;
+      }
+    });
+    pages[currentPage].classList.add('active');
+  };
 
-    }
+  $('.js-settings-button').click((e) => {
+    let target = e.target;
 
-    $('.js-settings-pages').click(() => {
-        console.log('!!!');
-    })
-
+    buttons.forEach((item) => {
+      item.classList.remove('settingsPages__button_active');
+    });
+    target.classList.add('settingsPages__button_active');
+    changePage(target.id);
+  });
 });
