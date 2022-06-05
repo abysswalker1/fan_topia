c7f529e1e9f5aef97fa959697e35f2320f3295df
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2022-06-06 01:27:26 +0500

    add-post-modal post-now

--- src/js/components/addPostModal.js
+++ src/js/components/addPostModal.js
@@ -1,24 +1,24 @@
-// import MicroModal from 'micromodal';
+//  import MicroModal from 'micromodal';
 
-(function() {
-  // MicroModal.init({
-  //   onShow: modal => console.info(`${modal.id} is shown`), // [1]
-  //   onClose: modal => console.info(`${modal.id} is hidden`), // [2]
-  //   openTrigger: 'data-custom-open', // [3]
-  //   closeTrigger: 'data-custom-close', // [4]
-  //   openClass: 'is-open',
-  //   disableScroll: true,
-  //   disableFocus: false,
-  //   awaitOpenAnimation: false,
-  //   awaitCloseAnimation: false,
-  //   debugMode: true
-  // });
+// (function() {
+//    MicroModal.init({
+//      onShow: modal => console.info(`${modal.id} is shown`),  
+//      onClose: modal => console.info(`${modal.id} is hidden`),  
+//      openTrigger: 'data-custom-open',  
+//      closeTrigger: 'data-custom-close',
+//      openClass: 'is-open',
+//      disableScroll: true,
+//      disableFocus: false,
+//      awaitOpenAnimation: false,
+//      awaitCloseAnimation: false,
+//      debugMode: true
+//    });
 
 
-  // const button = document.querySelector('.js-open-create-post');
-  // console.log(button)
-  // button.addEventListener('click', function(){
-  //   console.log("!!!!!!!!!1")
-  //   // MicroModal.show('modal-1');
-  // });
-})();
+//    const button = document.querySelector('.js-open-create-post');
+//    console.log(button)
+//    button.addEventListener('click', function(){
+//      console.log("!!!!!!!!!1")
+//       MicroModal.show('modal-1');
+//    });
+// })();
