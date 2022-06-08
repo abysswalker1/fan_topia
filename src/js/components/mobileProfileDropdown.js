5caa62e7a09bb91907a50cf8c21a3bf5ff9ff2cf
Evgeniia Philippova <evgeniia.philippova@vialet.eu>
2022-06-08 20:53:44 +0300

    Add profile dropdown

---
+++ src/js/components/mobileProfileDropdown.js
@@ -0,0 +1,8 @@
+(function() {
+  const $mobileProfileDropdown = $('#mobile-profile-dropdown');
+  const $trigger = $('#trigger-mobile-dropdown');
+
+  $trigger.click(function() {
+    $mobileProfileDropdown.show();
+  });
+})();
