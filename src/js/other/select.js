64f8220530499253956d0db2c7a11eb12114f655
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2022-06-10 04:11:49 +0500

    add-post-modal select

--- src/js/other/select.js
+++ src/js/other/select.js
@@ -6,4 +6,11 @@ import 'select2';
     closeOnSelect: false,
     width: '100%'
   });
+
+  $('#add-post-modal-select').select2({
+    minimumResultsForSearch: -1,
+    closeOnSelect: false,
+    width: '100%'
+  });
+
 })();
